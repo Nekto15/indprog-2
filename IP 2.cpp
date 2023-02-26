ffd0c86378504ad15ad49688b318f95bff8e0772
Nekto15 <86163121+Nekto15@users.noreply.github.com>
2023-02-26 22:06:32 +0300

    Create IP 2.cpp

---
+++ IP 2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "Triangle.h"
+#include "Circle.h"
+#include "Figure.h"
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	//// треугольники
+	//Triangle a[3];
+	//for (int i = 0; i < 3; i++) {
+	//	Triangle T;
+	//	int x, y, z;
+	//	cout << "Введите длины сторон треугольника # " << i << "\n";
+	//	cin >> x >> y >> z;
+	//	T.set(x, y, z);
+	//	a[i] = T;
+	//}
+
+	//for (int i = 0; i < 3; i++) {
+	//	a[i].show();
+	//}
+		
+
+	//// Круги 
+	//Circle a;
+	//Circle b;
+	//Circle c;
+	// 
+	//std::cout << a.square() << "\n";
+	//std::cout << b.triangle_around(5, 4, 3) << "\n";
+	//std::cout << c.triangle_in(5, 4, 3) << "\n";
+	//std::cout << a.check_circle(0.5, 0, 1) << "\n";
+
+	////Четырёхугольники 
+	//Figure a;
+	//Figure b;
+	//Figure c;
+
+	//a.show();
+	//std::cout << b.is_prug() << "\n";
+	//std::cout << c.is_square() << "\n";
+	//std::cout << a.is_romb() << "\n";
+	//std::cout << b.is_in_circle() << "\n";
+	//std::cout << c.is_out_circle();
+
+	return 0;
+}
